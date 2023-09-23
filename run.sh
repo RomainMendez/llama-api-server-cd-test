3d661486752c3c7380b81e46ecbf37002beec8d9
Rom1 <31048092+RomainMendez@users.noreply.github.com>
2023-09-23 14:12:28 +0200

    Force upgrade of underlying llama-cpp bindings

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 # Install dependencies
 pip install llama-api-server
+pip install llama-cpp-python --force-reinstall --upgrade --no-cache-dir
 
 # Run the server
 python -m llama_api_server --host=0.0.0.0
